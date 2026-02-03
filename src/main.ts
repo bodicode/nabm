4ebe97f6a7febaf3b1b3f8efc9dc566e1d0f93b4
bodicode <29.hoang.10@gmail.com>
2026-02-03 15:42:03 +0700

    feat: Initialize NestJS application with CORS and Swagger documentation.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('NABM Basketball API (Live)')
     .setDescription('The API description for Basketball Booking Platform')
